7e5e220820c3d93ec2ab72cc99f6c0a12c6304c2
pavlos <pavlidis-4@hotmail.com>
2019-03-20 11:39:07 +0200

    finished task needed to be tested with multiple clients requesting

--- db_client.py
+++ db_client.py
@@ -10,21 +10,29 @@ while True:
     print('Choose 2 in order to update a contact ')
     print('Choose 3 in order to search for a contact ')
     print('Choose 4 in order to delete a contact ')
-    print('Choose 5 if you want to leave ')
+    print('Choose 5 if you want to leave \n')
     user_selection = int(input())
 
     if user_selection == 1:
         name = input('Give name : ')
         email = input('Give email : ')
-        phone = input('Give phoner number : ')
-        connection_with_server.add_contact(name,email,phone)
-    if user_selection == 2:
-        pass
-    if user_selection == 3:
-        pass
-    if user_selection == 4:
-        pass
-    if user_selection == 5:
+        phone = input('Give phone number : ')
+        print(connection_with_server.add_contact(name, email, phone))
+    elif user_selection == 2:
+        contact_id = input('Give contact\'s id that you cant to modify : ')
+        name = input('Give name : ')
+        email = input('Give email : ')
+        phone = input('Give phone number : ')
+        print(connection_with_server.update_contact(contact_id, name, email, phone))
+    elif user_selection == 3:
+        print('You can search every value at every column')
+        search_value = input('Give value that you want to search : \n')
+        results = connection_with_server.search_contact(search_value)
+        [print(' '.join(map(str, item))) for item in results]
+    elif user_selection == 4:
+        contact_id = input('Give contact\'s id that you want to delete')
+        print(connection_with_server.delete_contact(contact_id))
+    elif user_selection == 5:
         break
     else:
         print('Wrong input, try again')
--- db_server.py
+++ db_server.py
@@ -7,6 +7,7 @@ import Pyro4
 
 sql_file = 'katanemimena.db'
 
+
 def find_db(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -22,52 +23,67 @@ class DataBaseConnection(object):
         self.__start_db()
 
     def __start_db(self):
+        self.conn = sqlite3.connect(sql_file)
+        self.c = self.conn.cursor()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS CONTACTS
+                   (ID INT PRIMARY KEY    NOT NULL,
+                   NAME           TEXT    NOT NULL,
+                   EMAIL          TEXT    NOT NULL,
+                   PHONE_NUMBER   TEXT    NOT NULL);''')
 
-        # check if db exist. If not create it
-        if len(find_db('*.db', './')) > 0:
-            self.conn = sqlite3.connect(sql_file)
-            self.c = self.conn.cursor()
-        else:
-            self.conn = sqlite3.connect(sql_file)
-            self.c = self.conn.cursor()
-            self.c.execute('''CREATE TABLE CONTACTS
-                     (ID INT PRIMARY KEY    NOT NULL,
-                     NAME           TEXT    NOT NULL,
-                     EMAIL          TEXT    NOT NULL,
-                     PHONE_NUMBER   TEXT    NOT NULL);''')
+        self.c.execute("select count(*) from CONTACTS")
+        is_empty = self.c.fetchone()[0]
+
+        if is_empty == 0:
+            self.c.execute("INSERT INTO CONTACTS (ID,NAME,EMAIL,PHONE_NUMBER) \
+                                      VALUES (1, 'Paul', 'temp1@gmail.com', 6979263755)")
 
             self.c.execute("INSERT INTO CONTACTS (ID,NAME,EMAIL,PHONE_NUMBER) \
-                          VALUES (1, 'Paul', 'temp1@gmail.com', 6979263755)")
+                                      VALUES (2, 'Maria', 'temp2@gmail.com', 6979242156)")
 
             self.c.execute("INSERT INTO CONTACTS (ID,NAME,EMAIL,PHONE_NUMBER) \
-                          VALUES (2, 'Maria', 'temp2@gmail.com', 6979242156)")
+                                      VALUES (3, 'John', 'temp3@gmail.com', 6987543245)")
 
             self.c.execute("INSERT INTO CONTACTS (ID,NAME,EMAIL,PHONE_NUMBER) \
-                          VALUES (3, 'John', 'temp3@gmail.com', 6987543245)")
-
-            self.c.execute("INSERT INTO CONTACTS (ID,NAME,EMAIL,PHONE_NUMBER) \
-                          VALUES (4, 'Nick', 'temp4@gmail.com', 6978432421)")
+                                      VALUES (4, 'Nick', 'temp4@gmail.com', 6978432421)")
             self.conn.commit()
 
-        # temp = self.conn.sqlExec('SELECT COUNT(*) * FROM {tn}'.format(tn=table_name))
-        temp = self.c.execute("select * from CONTACTS where id ==1")
-        print("->", len(temp.fetchall()), temp.fetchall())
-
     def add_contact(self, name,email,phone):
-        self.c.execute("INSERT INTO CONTACTS VALUES (NULL,?,?,?)", (name,email,phone))
+        try:
+            self.c.execute("select count(*) from CONTACTS")
+            max_id = self.c.fetchone()[0] + 1
+            self.c.execute("INSERT INTO CONTACTS VALUES (?,?,?,?)", (max_id, name, email, phone))
+            self.conn.commit()
+        except:
+            return 'Something went wrong. Try again'
+        return 'Add done'
 
-    def update_contact(self):
-        self.x = 0
+    def update_contact(self, contact_id, name, email, phone):
+        try:
+            self.c.execute("""UPDATE CONTACTS SET name = ?, EMAIL = ?, PHONE_NUMBER = ? WHERE ID=? """, \
+                       (name, email, phone, contact_id))
+            self.conn.commit()
+        except:
+            return 'Something went wrong. Try again'
+        return 'Update done'
 
-    def search_contact(self):
-        self.x = 0
+    def search_contact(self, search_value):
+        final_search_value = '%' + search_value + '%'
+        self.c.execute("""SELECT * FROM CONTACTS WHERE NAME like ? or EMAIL like ? or PHONE_NUMBER like ? """, \
+                       (final_search_value, final_search_value, final_search_value))
+        results = self.c.fetchall()
+        return results
 
-    def delete_contact(self):
-        self.x = 0
+    def delete_contact(self,contact_id):
+        try:
+            self.c.execute('''DELETE FROM CONTACTS where ID =? ''', (contact_id))
+        except:
+            return 'Something went wrong. Try again'
+        self.conn.commit()
+        return 'Delete done'
 
 
 if __name__ == '__main__':
-    x = DataBaseConnection()
     daemon = Pyro4.Daemon()  # make a Pyro daemon
     ns = Pyro4.locateNS()  # find the name server
     uri = daemon.register(DataBaseConnection)  # register the greeting maker as a Pyro object
